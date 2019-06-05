bba41148aedc91d5d8cc94e3750fe0f2f2ca65e5
chalimbu <sebitaszapata12@gmail.com>
2019-06-05 09:02:48 -0500

    finished the crud operation

--- src/index.js
+++ src/index.js
@@ -49,6 +49,35 @@ app.get('/users/:id', async(req, res) => {
     }
 
 })
+app.patch('/users/:id', async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every(updates => allowUpdates.includes(updates))
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/users/:id', async(req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 
 app.post('/task', async(req, res) => {
@@ -83,6 +112,37 @@ app.get('/tasks/:id', async(req, res) => {
         res.status(500).send(e)
     }
 })
+
+app.patch('/tasks/:id', async(req, res) => {
+    const updates = Object.keys(req.body)
+    const validUpdate = ['description', 'completed']
+    const areValidUpdates = updates.every(param => validUpdate.includes(param))
+    if (!areValidUpdates) {
+        return res.status(400).send({ error: 'invalid paramaters' })
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send()
+        }
+        return res.send(task)
+    } catch (e) {
+        return res.status(500).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async(req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
